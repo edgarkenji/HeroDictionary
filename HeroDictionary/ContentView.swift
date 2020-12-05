af77694b68470ca087d9ce469f150fa508f2c53f
Edgar Kenji Yamamoto <edgar.yamamoto@gmail.com>
2020-12-05 14:57:53 -0300

    Add initial files

---
+++ HeroDictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeroDictionary
+//
+//  Created by Edgar Kenji Yamamoto on 05/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeroDictionary/HeroDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroDictionaryApp.swift
+//  HeroDictionary
+//
+//  Created by Edgar Kenji Yamamoto on 05/12/20.
+//
+
+import SwiftUI
+
+@main
+struct HeroDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
